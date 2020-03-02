2ddf0e997665a46073b9a32a440fd07157777cd6
Safdar Rao <safdarchuhan786@outlook.com>
2020-03-02 21:19:29 +0500

    commit

--- Basic Assigment/wwwroot/js/studentForm.js
+++ Basic Assigment/wwwroot/js/studentForm.js
@@ -10,68 +10,112 @@ function onFormSubmit() {
         resetForm();
     }
 }
-function readFormData() {
-    var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["FthName"] = document.getElementById("FthName").value;
-    formData["CNIC"] = document.getElementById("CNIC").value;
-    formData["course"] = document.getElementById("course").value;
-    return formData;
-}
-function insertNewRecord(data) {
-    var table = document.getElementById("studentList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.FthName;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.CNIC;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.course;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
-}
-
-function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("FthName").value = "";
-    document.getElementById("CNIC").value = "";
-    document.getElementById("course").value = "";
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("FthName").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("CNIC").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("course").value = selectedRow.cells[3].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.FthName;
-    selectedRow.cells[2].innerHTML = formData.CNIC;
-    selectedRow.cells[3].innerHTML = formData.course;
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("studentList").deleteRow(row.rowIndex);
-        resetForm();
+    function readFormData() {
+        var formData = {};
+        formData["fullName"] = document.getElementById("fullName").value;
+        formData["FthName"] = document.getElementById("FthName").value;
+        formData["CNIC"] = document.getElementById("CNIC").value;
+        formData["phone"] = document.getElementById("phone").value;
+        formData["course"] = document.getElementById("course").value;
+        formData["male"] = document.getElementById("male").value;
+        formData["female"] = document.getElementById("female").value;
+        return formData;
     }
+    function insertNewRecord(data) {
+        var table = document.getElementById("studentList").getElementsByTagName('tbody')[0];
+        var newRow = table.insertRow(table.length);
+        cell1 = newRow.insertCell(0);
+        cell1.innerHTML = data.fullName;
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.FthName;
+        cell3 = newRow.insertCell(2);
+        cell3.innerHTML = data.CNIC;
+        cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.phone;
+        cell5 = newRow.insertCell(4);
+        cell5.innerHTML = data.course;
+        cell6 = newRow.insertCell(5);
+        cell6.innerHTML = data.male;
+        cell6.innerHTML = data.female;
+        cell7 = newRow.insertCell(6);
+        cell7.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+    }
+    function resetForm() {
+        document.getElementById("fullName").value = "";
+        document.getElementById("FthName").value = "";
+        document.getElementById("CNIC").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("course").value = "";
+
+        selectedRow = null;
+    }
+
+    function onEdit(td) {
+        selectedRow = td.parentElement.parentElement;
+        document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+        document.getElementById("FthName").value = selectedRow.cells[1].innerHTML;
+        document.getElementById("CNIC").value = selectedRow.cells[2].innerHTML;
+        document.getElementById("phone").value = selectedRow.cells[3].innerHTML;
+        document.getElementById("course").value = selectedRow.cells[4].innerHTML;
+    }
+    function updateRecord(formData) {
+        selectedRow.cells[0].innerHTML = formData.fullName;
+        selectedRow.cells[1].innerHTML = formData.FthName;
+        selectedRow.cells[2].innerHTML = formData.CNIC;
+        selectedRow.cells[3].innerHTML = formData.phone;
+        selectedRow.cells[4].innerHTML = formData.course;
+
+    }
+
+    function onDelete(td) {
+        if (confirm('Are you sure to delete this record ?')) {
+            row = td.parentElement.parentElement;
+            document.getElementById("studentList").deleteRow(row.rowIndex);
+            resetForm();
+        }
 }
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
+    if (document.getElementById("fullName").value !== "^[A-Za-z]") {
         document.getElementById("fullNameValidationError").classList.remove("hide");
+        isValid = false;
     } else {
         isValid = true;
         if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
             document.getElementById("fullNameValidationError").classList.add("hide");
+    } if (document.getElementById("FthName").value == "") {
+        document.getElementById("fthNameValidationError").classList.remove("hide");
+        isValid = false;
+    } else {
+        isValid = true;
+        if (!document.getElementById("fthNameValidationError").classList.contains("hide"))
+            document.getElementById("fthNameValidationError").classList.add("hide");
+
+    } if (document.getElementById("CNIC").value == "") {
+        document.getElementById("CNICvalidationError").classList.remove("hide");
+        isValid = false;
+    } else {
+        isValid = true;
+        if (!document.getElementById("CNICvalidationError").classList.contains("hide"))
+            document.getElementById("CNICvalidationError").classList.add("hide");
+
+    } if (document.getElementById("phone").value == "") {
+        document.getElementById("phonevalidationError").classList.remove("hide");
+        isValid = false;
+    } else {
+        isValid = true;
+        if (!document.getElementById("phonevalidationError").classList.contains("hide"))
+            document.getElementById("phonevalidationError").classList.add("hide");
+
+    //} if (document.getElementById("course").value == "select") {
+    //    document.getElementById("courseValidationError").classList.remove("hide");
+    //    isValid = false;
+    //} else {
+    //    isValid = true;
+    //    if (!document.getElementById("courseValidationError").classList.contains("hide"))
+    //        document.getElementById("courseValidationError").classList.add("hide");
     }
-    return isValid;
-}
+        return isValid;
+    }
+
